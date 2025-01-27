fb71788935e3eff2b6d830246be1a576f3b3230c
rafael notebook <rafa_an_jo@hotmail.com>
2025-01-27 00:02:43 -0300

    resolução de comflitos na branch

--- include/velha.hpp
+++ include/velha.hpp
@@ -23,8 +23,8 @@ class Velha:public Jogos{
         bool verificaJogada(int linha, int coluna, char valor, std::string jogador) override;
         void criaTabuleiro() override;
         bool verificaGanhador(std::string jogador1, std::string jogador2, char valor) override;
-        bool verificaDiagonais(const std::string& estado, char valor);
-        bool verificaColuna(const std::string& estado, int coluna, char valor);
+        bool verificaDiagonais(const std::string& estado);
+        bool verificaColuna(const std::string& estado, int coluna);
         bool verificaLinha(const std::string& estado, int linha);
         bool verificaTabLimpo(std::string estado);
         bool jogada(std::string jogador1, std::string jogador2, char valor);
--- src/velha.cpp
+++ src/velha.cpp
@@ -127,28 +127,67 @@ bool Velha::verificaJogada(int linha, int coluna, char valor, std::string jogado
 }
 
 
+//verifica se digitou apenas um numero
+bool Velha::verificaJogada(int linha, int coluna, char valor, std::string jogador){
+    std::vector<std::vector<char>> grid = tabuleiro.getGrid();
+    linha = linha-1;
+    coluna = coluna-1;
+
+    if ((linha < 0 || linha >= 3) || (coluna < 0 || coluna >= 3)) {
+        std::cerr << jogador << " posição inválida, tente novamente:";
+        return false;
+    }
+    else if (grid[linha][coluna * 2 + 1] != ' '){
+        std::cerr << jogador << " posição inválida, tente novamente: ";
+        return false;
+    }else {
+        tabuleiro.atualizarCelula(linha,coluna,valor);
+        tabuleiro.exibirTabuleiro();
+        return true;
+    }
+
+}
+
+
 bool Velha::verificaLinha(const std::string& estado, int linha) {
     int inicio = linha * 3;
-    return (estado[inicio] == valor && estado[inicio + 1] == valor && estado[inicio + 2] == valor);
+    if (estado[inicio] != ' ' &&
+        estado[inicio] == estado[inicio + 1] &&
+        estado[inicio + 1] == estado[inicio + 2]) {
+        vencedor = estado[inicio];
+        return true;
+    }
+    return false;
 }
 
-
-bool Velha::verificaColuna(const std::string& estado, int coluna, char valor) {
-    return (estado[coluna] == valor && estado[coluna + 3] == valor && estado[coluna + 6] == valor);
+bool Velha::verificaColuna(const std::string& estado, int coluna) {
+    if (estado[coluna] != ' ' &&
+        estado[coluna] == estado[coluna + 3] &&
+        estado[coluna + 3] == estado[coluna + 6]) {
+        vencedor = estado[coluna];
+        return true;
+    }
+    return false;
 }
 
-
-bool Velha::verificaDiagonais(const std::string& estado, char valor) {
-    return ((estado[0] == valor && estado[4] == valor && estado[8] == valor) ||
-            (estado[2] == valor && estado[4] == valor && estado[6] == valor));
+bool Velha::verificaDiagonais(const std::string& estado) {
+    if ((estado[0] != ' ' && estado[0] == estado[4] && estado[4] == estado[8]) ||
+        (estado[2] != ' ' && estado[2] == estado[4] && estado[4] == estado[6])) {
+        vencedor = estado[4];
+        return true;
+    }
+    return false;
 }
-
 
 bool Velha::verificaGanhador(std::string jogador1, std::string jogador2, char valor) {
     std::string estado = tabuleiro.getEstadoLimpo();
 
     for (int i = 0; i < 3; ++i) {
-        if (verificaLinha(estado, i, valor) || verificaColuna(estado, i, valor) || verificaDiagonais(estado, valor)) {
+        if (verificaLinha(estado, i) || verificaColuna(estado, i)||verificaDiagonais(estado)) {
+            std::cout << jogador1 << " ganhou com " << valor <<  std::endl;
+            manager.AddVit(jogador1, "velha");
+            manager.AddDer(jogador2, "velha");
+
             return true;
         }
     }
@@ -169,7 +208,6 @@ bool Velha::verificaGanhador(std::string jogador1, std::string jogador2, char va
 
     return false;
 }
-
 
 Velha::~Velha(){
 
